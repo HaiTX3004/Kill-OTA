15216a9602084a0671980e289e513685501082e7
HaiTX3004 <100550537+HaiTX3004@users.noreply.github.com>
2023-07-14 16:34:44 +0700

    Update BlockOTA folder

---
+++ KillOTA/BlockOTA/Blocker.h
@@ -0,0 +1,14 @@
+//
+//  Blocker.h
+//  
+//
+//  Created by HaiTX3004.
+//
+
+#ifndef Blocker_h
+#define Blocker_h
+
+#include <stdio.h>
+void enableOTA(void);
+void disableOTA(void);
+#endif /* Blocker_h */
